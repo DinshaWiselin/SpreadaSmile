a9ada17c73c1010c28d837fe0bb2d53128af5799
dinshawiselinchristudhas <dinsha.wiselin@gmail.com>
2023-06-22 10:45:39 +0200

    Initial Commit

---
+++ SpreadaSmile/SpreadaSmileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpreadaSmileApp.swift
+//  SpreadaSmile
+//
+//  Created by Dinsha Wiselin Christudhas on 22.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SpreadaSmileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
